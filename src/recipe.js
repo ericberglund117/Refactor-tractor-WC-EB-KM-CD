0f121cd2b82f29010963fdd621f7fe154e9e57c4
Eric Berglund <ericberglund117@gmail.com>
2020-09-06 15:43:03 -0600

    Refactor calculateIngredientsCost function. Still needs work"

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,5 @@
+import ingredientsData from '../src/data/ingredient-Data.js'
+
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
@@ -5,12 +7,35 @@ class Recipe {
     this.image = recipe.image;
     this.tags = recipe.tags;
     this.ingredients = recipe.ingredients;
-  }
-  calculateIngredientsCost() {
-    // return this.ingredients.map(i => {
-    //   ingredientData.find(ingredient => ingredient === i);
+    // this.ingredientsData =
+  };
+
+  calculateIngredientsCost(ingredientsData) {
+    //   let match = this.ingredients.map(i => {
+    //     console.log(i)
+    //     ingredientsData.find(ingredient => ingredient.id === i.id);
     // });
-  }
+    // console.log(match)
+    let sum = 0;
+    let match = this.ingredients.forEach(recipeIngredient => {
+      // Object.entries(recipeIngredient)
+      let anything = ingredientsData.find(ingredient => ingredient.id === recipeIngredient.id)
+      console.log(anything)
+      // ingredient.id === ingredientsData[index].id
+    })
+    // console.log(match)
+
+    // let cost = this.ingredients.reduce((sum, currentIngredient) => {
+    //   // console.log(currentIngredient)
+    //     sum += ingredient.estimatedCostInCents
+    //   return sum;
+    // }, 0)
+
+    // console.log(cost)
+  };
+  //input- this.ingredients(recipe ingredients) && ingredientsData
+  //output - return sum of ingredient costs of recipe
+  //methods - map, reduce
 }
 
-module.exports = Recipe;
+export default Recipe;
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -1,14 +1,17 @@
 import { expect } from 'chai';
 
 import Recipe from '../src/recipe';
-// import data from '../data/recipe-data';
+
 
 describe('Recipe', function() {
   let recipe;
+  let recipe1;
   let recipeInfo;
+  let recipeInfo1;
+  let ingredientsDataTest;
 
   beforeEach(function() {
-    recipeInfo = [
+    recipeInfo =
       {
         "name": "Loaded Chocolate Chip Pudding Cookie Cups",
         "id": 595736,
@@ -31,8 +34,9 @@ describe('Recipe', function() {
             }
           }
         ]
-      },
+      };
 
+    recipeInfo1 =
       {
         "name": "Maple Dijon Apple Cider Grilled Pork Chops",
         "id": 678353,
@@ -55,9 +59,23 @@ describe('Recipe', function() {
             }
           }
         ]
-      }
-    ];
+      };
+
+      ingredientsDataTest = [
+        {
+          "id": 20081,
+          "name": "wheat flour",
+          "estimatedCostInCents": 142
+        },
+        {
+          "id": 18372,
+          "name": "bicarbonate of soda",
+          "estimatedCostInCents": 582
+        }
+      ];
+
     recipe = new Recipe(recipeInfo);
+    recipe1 = new Recipe(recipeInfo1)
   })
 
   it('is a function', function() {
@@ -69,15 +87,15 @@ describe('Recipe', function() {
   });
 
   it('should initialize with an id', function() {
-    expect(recipe.id).to.eq(595736);
+    expect(recipe.id).to.equal(595736);
   });
 
   it('should initialize with an name', function() {
-    expect(recipe.name).to.eq('Loaded Chocolate Chip Pudding Cookie Cups');
+    expect(recipe.name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
   });
 
   it('should initialize with an image', function() {
-    expect(recipe.image).to.eq('https://spoonacular.com/recipeImages/595736-556x370.jpg');
+    expect(recipe.image).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg');
   });
 
   it('should initialize with an array of ingredients', function() {
@@ -93,6 +111,6 @@ describe('Recipe', function() {
   });
 
   it('should calculate the total cost of all of the ingredients', function() {
-    expect(recipe.calculateIngredientsCost()).to.eq();
+    expect(recipe.calculateIngredientsCost(ingredientsDataTest)).to.eq(726);
   });
 });
