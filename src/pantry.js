292349b5f6f0fbef55690b14357d498adb39454f
Kristi Miller <kristi.ann.miller@gmail.com>
2020-09-09 17:32:43 -0600

    Add functionality to determineIngredientsAvailable method

--- src/pantry.js
+++ src/pantry.js
@@ -1,9 +1,27 @@
 class Pantry {
   constructor(userID, pantryArray) {
     this.id = userID
-    this.contents = pantryArray;
+    this.contents = pantryArray
   };
-};
+
+  determineIngredientsAvailable(recipe) {
+    // let match = recipe.ingredients.map(recipeIngredient => {
+    //   return this.contents.find(item => item.ingredient === recipeIngredient.id)
+    // })
+    // console.log(match)
+    // return match.includes(undefined) ? false : true
+    return recipe.ingredients.every(recipeIng => {
+      let match = this.contents.find(item => item.ingredient === recipeIng.id)
+      if(match === undefined) {
+        return false
+      }
+      return recipeIng.quantity.amount <= match.amount
+    })
+
+  }
+
+}
+
 
 
 
