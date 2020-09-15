1679a28bd36f4461330292b1873fbc850fac92b1
Kristi Miller <kristi.ann.miller@gmail.com>
2020-09-14 20:44:14 -0600

    Add describe blocks for organizaion and fix minor lint errors

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -4,7 +4,6 @@ import Recipe from '../src/recipe';
 
 describe('Recipe', function() {
   let recipe;
-  let recipe1;
   let recipeInfo;
   let recipeInfo1;
   let ingredientsDataTest;
@@ -104,57 +103,65 @@ describe('Recipe', function() {
       ];
 
     recipe = new Recipe(recipeInfo);
-    recipe1 = new Recipe(recipeInfo1)
   })
+  describe('Properties and functionality', function() {
+    it('is a function', function() {
+      expect(Recipe).to.be.a('function');
+    });
 
-  it('is a function', function() {
-    expect(Recipe).to.be.a('function');
-  });
+    it('should be an instance of Recipe', function() {
+      expect(recipe).to.be.an.instanceof(Recipe);
+    });
 
-  it('should be an instance of Recipe', function() {
-    expect(recipe).to.be.an.instanceof(Recipe);
-  });
+    it('should initialize with an id', function() {
+      expect(recipe.id).to.equal(595736);
+    });
 
-  it('should initialize with an id', function() {
-    expect(recipe.id).to.equal(595736);
-  });
+    it('should initialize with an name', function() {
+      expect(recipe.name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
+    });
 
-  it('should initialize with an name', function() {
-    expect(recipe.name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups');
-  });
+    it('should initialize with an image', function() {
+      expect(recipe.image).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg');
+    });
 
-  it('should initialize with an image', function() {
-    expect(recipe.image).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg');
-  });
-
-  it('should initialize with an array of ingredients', function() {
-    const ingredient = {
-      "id": 20081,
-      "name": "all purpose flour",
-      "quantity": {
-        "amount": 1.5,
-        "unit": "c"
+    it('should initialize with an array of ingredients', function() {
+      const ingredient = {
+        "id": 20081,
+        "name": "all purpose flour",
+        "quantity": {
+          "amount": 1.5,
+          "unit": "c"
+        }
       }
-    }
-    expect(recipe.ingredients[0]).to.deep.equal(ingredient);
+      expect(recipe.ingredients[0]).to.deep.equal(ingredient);
+    });
   });
 
-  it('should calculate the total cost of all of the ingredients', function() {
-    expect(recipe.calculateIngredientsCost(ingredientsDataTest)).to.equal(504);
+  describe('calculateIngredientsCost', function() {
+    it('should calculate the total cost of all of the ingredients', function() {
+      expect(recipe.calculateIngredientsCost(ingredientsDataTest)).to.equal(504);
+    });
   });
 
-  it('should be able to filter recipes by tag/type', function() {
-    expect(recipe.filterAllRecipes(recipeInfo, 'starter')).to.equal(recipeInfo);
-    expect(recipe.filterAllRecipes(recipeInfo1, 'dinner')).to.equal(recipeInfo1);
+  describe('filterAllRecipes', function() {
+    it('should be able to filter recipes by tag/type', function() {
+      expect(recipe.filterAllRecipes(recipeInfo, 'starter')).to.equal(recipeInfo);
+      expect(recipe.filterAllRecipes(recipeInfo1, 'dinner')).to.equal(recipeInfo1);
+    });
   });
 
-  it('should be able to search recipes by ingredients', function() {
-    expect(recipe.searchRecipes(recipeInfo, 'baking soda')).to.equal(recipeInfo);
-    expect(recipe.searchRecipes(recipeInfo1, 'apple cider')).to.equal(recipeInfo1);
+  describe('searchRecipes', function() {
+    it('should be able to search recipes by ingredients', function() {
+      expect(recipe.searchRecipes(recipeInfo, 'baking soda')).to.equal(recipeInfo);
+      expect(recipe.searchRecipes(recipeInfo1, 'apple cider')).to.equal(recipeInfo1);
+    });
   });
 
-  it('the getInstructions function should return the recipe instructions', function() {
-    expect(recipe.getInstructions(recipeInfo)).to.equal(recipeInfo.instructions);
-    expect(recipe.getInstructions(recipeInfo1)).to.equal(recipeInfo1.instructions);
-  })
+  describe('getInstructions', function() {
+    it('should return the recipe instructions', function() {
+      expect(recipe.getInstructions(recipeInfo)).to.equal(recipeInfo.instructions);
+      expect(recipe.getInstructions(recipeInfo1)).to.equal(recipeInfo1.instructions);
+    });
+  });
 });
