65d0fdb03edb343a572a756d816d2cb46428e6f7
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-09-14 19:34:44 -0600

    Organize and lint recipe.js

--- src/recipe.js
+++ src/recipe.js
@@ -4,44 +4,44 @@
 
 class Recipe {
   constructor(recipe) {
-    // super()
     this.id = recipe.id;
     this.name = recipe.name;
     this.image = recipe.image;
     this.tags = recipe.tags;
     this.ingredients = recipe.ingredients;
-  };
+  }
 
   calculateIngredientsCost(ingredientsData) {
     let cost = this.ingredients.map(recipeIngredient => {
       let recipeAmount = recipeIngredient.quantity.amount;
-      let match = ingredientsData.find(ingredient => ingredient.id === recipeIngredient.id)
-      return match.estimatedCostInCents * recipeAmount
+      let match = ingredientsData.find(ingredient =>
+        ingredient.id === recipeIngredient.id);
+      return match.estimatedCostInCents * recipeAmount;
     })
     let totalCost = cost.reduce((sum, ingredientCost) => {
-      sum += ingredientCost
-      return sum
+      sum += ingredientCost;
+      return sum;
     }, 0);
-    return totalCost
-  };
+    return totalCost;
+  }
 
   filterAllRecipes(recipe, tag) {
-    if(recipe.tags.includes(tag)) {
+    if (recipe.tags.includes(tag)) {
       return recipe
     }
- };
+  }
 
- searchRecipes(recipe, ingredient) {
-   let ingredientsValues = Object.values(recipe.ingredients)
-   let ingredientsList = ingredientsValues.map(ingredient => ingredient.name)
-   if(ingredientsList.includes(ingredient)) {
-     return recipe
-   }
- };
+  searchRecipes(recipe, ingredient) {
+    let ingredientsValues = Object.values(recipe.ingredients)
+    let ingredientsList = ingredientsValues.map(ingredient => ingredient.name)
+    if (ingredientsList.includes(ingredient)) {
+      return recipe
+    }
+  }
 
-getInstructions(recipe) {
-  return recipe.instructions
- };
-};
+  getInstructions(recipe) {
+    return recipe.instructions
+  }
+}
 
 export default Recipe;
